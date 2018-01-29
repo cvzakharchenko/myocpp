7a3ef5ec74277cca8b4d4dba55c7ce63f4544d19
cvzakharchenko <cvzakharchenko@gmail.com>
2018-01-29 09:24:29 +0300

    Initial. YAML file and test code added.

---
+++ test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string> // sdfsdfsdfsdf
+asasas
+dasdasd
+
+class xyz
+{
+public:
+	xyz(int x)
+		: x { x }
+	{
+
+	}
+
+int x;
+};
+
+int function(int a)
+{
+	return a+21;
+}
+
+int xxxxx;
+
+int main(void)
+{
+	std::string s;
+	int xyz;
+	int test;
+
+	s.reserve(function(123));
+
+	std::vector<std::string> x{fun(123)};
+	xyz yqwe{45};
+
+	asdasd::qewqwe asdsd();
+
+	qweqweqw::eqweqw qweqwe;
+
+	asdsd += 45;
+
+	return 0;
+}
